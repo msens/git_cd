e6491911549a1b452470504a089391ff271e3f4a
Michiel Sens <msens@xebia.com>
2013-10-29 10:29:27 +0100

    new test, MANIFEST INCLUDED

---
+++ demo-integration-test/src/main/java/bogus/BogusCode.java
@@ -0,0 +1,11 @@
+package bogus;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michielsens
+ * Date: 29/10/2013
+ * Time: 10:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class BogusCode {
+}
