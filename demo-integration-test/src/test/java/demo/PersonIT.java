c2a47e2d4106d35b9ab22872ddbbb851694acbc9
Michiel Sens <msens@xebia.com>
2013-11-07 16:01:06 +0100

    new test, MANIFEST INCLUDED

--- demo-integration-test/src/test/java/demo/PersonIT.java
+++ demo-integration-test/src/test/java/demo/PersonIT.java
@@ -9,7 +9,7 @@ public class PersonIT {
 
     @Test
     public void testPersonService() {
-        expect().body("firstName", equalTo("John")).when().get("http://localhost:1234/MyWebApp/rest/person/John/Doe");
+        expect().body("firstName", equalTo("John")).when().get("http://localhost:9090/MyWebApp/rest/person/John/Doe");
 //      expect().body("firstName", equalTo("John")).when().get("http://localhost:1234/rest/person/John/Doe");
 //    with().parameters("firstName", "John", "lastName", "Doe").expect().body("firstName", equalTo("John")).
 //            body("lastName", equalTo("Doe")).when().get("http://localhost:8080/rest/person");
