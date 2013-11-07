e616a52af5106f02a92356afe00db9c06408d1e4
Michiel Sens <msens@xebia.com>
2013-11-07 16:28:30 +0100

    new test, MANIFEST INCLUDED

--- demo-integration-test/src/test/java/demo/PersonIT.java
+++ demo-integration-test/src/test/java/demo/PersonIT.java
@@ -9,6 +9,7 @@ public class PersonIT {
 
     @Test
     public void testPersonService() {
+        // note: context root and port needs to be equal to what is defined in pom file!
         expect().body("firstName", equalTo("John")).when().get("http://localhost:9090/rest/person/John/Doe");
 //      expect().body("firstName", equalTo("John")).when().get("http://localhost:1234/rest/person/John/Doe");
 //    with().parameters("firstName", "John", "lastName", "Doe").expect().body("firstName", equalTo("John")).
